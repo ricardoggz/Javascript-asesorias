d4c07769d81a7579cb830f73be1a496e68971d42
ricardoggz <ricardo_ggzicm@outlook.com>
2023-01-24 21:45:21 -0600

    Complementacion de arreglos

--- arreglos/index.js
+++ arreglos/index.js
@@ -31,3 +31,63 @@ objects.map((object)=> console.log(object.name))
 //forEach : solo recorre el arreglo
 const objects2 = [{name : 'Ricardo'},{name: 'Alexis'}]
 objects.forEach((object)=> console.log(object))
+
+//recorrer un arreglo de mayor longitug
+const users = [
+    {
+       id: 1,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496"
+        }
+      }
+    },
+    {
+        id: 2,
+         name: "Ricardo Guevara",
+         username: "Bret",
+         email: "Sincere@april.biz",
+         address: {
+         street: "Kulas Light",
+         suite: "Apt. 556",
+         city: "Gwenborough",
+         zipcode: "92998-3874",
+         geo: {
+           lat: "-37.3159",
+           lng: "81.1496"
+         }
+       }
+     }      
+]
+//Recorrer el arreglo con map
+users.map((elemento)=>console.log(elemento))
+//Recorrer el arreglo con forEach
+users.forEach((elemento)=>console.log(elemento))
+//Recorrer el arreglo con ciclo for
+for(let i = 0; i <=users.length; i++){
+    console.log(users[i])
+}
+//Recorrer arreglo con while
+let i = 0
+while(i<=users.length){
+    console.log(users[i])
+    i++
+}
+//arreglos multidimensionales
+const multi = [
+    [
+        ['Diego']
+    ]
+]
+console.log(multi[0][0][0])
+const [first] = multi
+const [second] = first
+console.log(second[0])
